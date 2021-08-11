a7efbdff36ccb314c05aec0322f0565c079f1cd7
AlexGuiSag <aleguitronsaga@gmail.com>
2021-08-11 12:22:56 -0500

    feat: blank below the header. Cause there are not any todos to display

--- lib/live_view_todis_web/live/todo_live.ex
+++ lib/live_view_todis_web/live/todo_live.ex
@@ -1,11 +1,8 @@
 defmodule LiveViewTodisWeb.TodoLive do
   use LiveViewTodisWeb, :live_view
+alias LiveViewTodis.Todos
 
-  def mount(_params, _session, socket) do
-    {:ok, socket}
-  end
-
-  def render(assigns) do
-    ~L"Rendering LiveView"
-  end
+def mount(_params, _session, socket) do
+  {:ok, assign(socket, todos: Todos.list_todos())}
+end
 end
